fb74043160c2cdb64303becb1651914a6025ca33
sergeiKalinin35 <piolersa85@mail.ru>
2022-11-24 13:10:47 +0200

    slider

--- HOME-3/ViewController.swift
+++ HOME-3/ViewController.swift
@@ -8,12 +8,58 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet var viewColor: UIView!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+//        
+//        // Slider
+//        slider.value = 1
+//        slider.minimumValue = 0// alpha view
+//        slider.maximumValue = 1
+//        slider.minimumTrackTintColor = .black
+//        slider.maximumTrackTintColor = .white
+//        slider.thumbTintColor = .systemYellow
+//        
+//        
+//        
+//        
+//        
+//        // Label
+//   //     mainLabel.text = ""
+//        mainLabel.text = String(slider.value)
+        
+        
+        
+       
     }
 
 
+//
+//    @IBAction func sliderAction() {
+//        let currentValue = CGFloat(slider.value)
+//        mainLabel.text = String(slider.value)
+//        view.backgroundColor =  view.backgroundColor?.withAlphaComponent(currentValue)
+//    }
+    
+    
+    
+    
+    
+    
+    
 }
 
