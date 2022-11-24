2f510803419a955b0547367ec47db568e0ca7747
sergeiKalinin35 <piolersa85@mail.ru>
2022-11-24 12:25:17 +0200

    Initial Commit

---
+++ HOME-3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HOME-3
+//
+//  Created by Sergey on 24.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
